64d8e555594a92fc4f0f8df854feb418c33d6462
Braden Holmes <bradenholmes92@Bradens-MacBook-Pro.local>
2017-02-23 09:51:49 -0500

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,15 @@
 console.log('wowow')
+import Backbone from 'backbone';
 import $ from 'jquery';
 
 $.getJSON('/proxy?api=http://www.bbc.co.uk/radio1/playlist.json').then(function(serverRes){
 	console.log(serverRes)
+})
+
+const AppRouter = Backbone.Router.extend({
+	initialize: function(){
+		Backbone.history.start()
+		console.log('app is routing!')
+	},
+	
 })
